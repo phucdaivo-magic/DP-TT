958672ce512b87a8ff3e5be6d95eef753daace57
phucdaivo-magic <daihandaiphuc@gmail.com>
2026-07-14 02:30:04 +0700

    d

--- src/js/thiep-moi-vu-quy.js
+++ src/js/thiep-moi-vu-quy.js
@@ -1,1 +1,16 @@
 import '../scss/thiep-moi-vu-quy.scss';
+
+const params = new URLSearchParams(location.search);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll("[data-name]").forEach(el => {
+        const names = {
+            name: 'Quý khách'
+        }
+        const value = params.get(el.dataset.name) || names[el.dataset.name] || '';
+        if (value) {
+            el.textContent = value;
+        }
+    });
+});
