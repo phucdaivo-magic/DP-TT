b958e594a5f43be67a670112ae79b83ef34e1862
phucdaivo-magic <daihandaiphuc@gmail.com>
2026-07-14 19:40:54 +0700

    abc

--- src/js/thiep-moi-vu-quy.js
+++ src/js/thiep-moi-vu-quy.js
@@ -1,5 +1,19 @@
 import '../scss/thiep-moi-vu-quy.scss';
 
+import EmblaCarousel from 'embla-carousel'
+// import { addDotButtonAndClickHandlers } from './EmblaCarouselDotButton'
+// import { addPrevNextButtonClickHandlers } from './EmblaCarouselArrowButtons'
+// import '../css/base.css'
+// import '../css/sandbox.css'
+// import '../css/embla.css'
+
+const OPTIONS = { axis: 'x' }
+
+const viewportNode = document.querySelector('.album__container')
+
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS)
+
+
 const params = new URLSearchParams(location.search);
 
 
