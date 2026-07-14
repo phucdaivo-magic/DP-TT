9494d659f1cd39507a9ee4f2a7b88fe89f86bc2b
phucdaivo-magic <daihandaiphuc@gmail.com>
2026-07-14 21:59:22 +0700

    Update code

--- src/js/thiep-moi-vu-quy.js
+++ src/js/thiep-moi-vu-quy.js
@@ -1,30 +1,45 @@
-import '../scss/thiep-moi-vu-quy.scss';
+import "../scss/thiep-moi-vu-quy.scss";
+import EmblaCarousel from "embla-carousel";
+import Autoplay from "embla-carousel-autoplay";
+import musicUrl from '@/media/le-duong.mp3';
 
-import EmblaCarousel from 'embla-carousel'
-// import { addDotButtonAndClickHandlers } from './EmblaCarouselDotButton'
-// import { addPrevNextButtonClickHandlers } from './EmblaCarouselArrowButtons'
-// import '../css/base.css'
-// import '../css/sandbox.css'
-// import '../css/embla.css'
+const viewportNode = document.querySelector(".album__container");
 
-const OPTIONS = { axis: 'x' }
-
-const viewportNode = document.querySelector('.album__container')
-
-const emblaApi = EmblaCarousel(viewportNode, OPTIONS)
-
+const emblaApi = EmblaCarousel(
+    viewportNode,
+    {
+        axis: "x",
+        loop: true,
+    },
+    [Autoplay({
+        delay: 3000,
+        playOnInit: true,
+    })],
+);
 
 const params = new URLSearchParams(location.search);
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll("[data-name]").forEach(el => {
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("[data-name]").forEach((el) => {
         const names = {
-            name: 'Quý khách'
-        }
-        const value = params.get(el.dataset.name) || names[el.dataset.name] || '';
+            name: "Quý khách",
+        };
+        const value = params.get(el.dataset.name) || names[el.dataset.name] || "";
         if (value) {
             el.textContent = value;
         }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const music = document.getElementById('music');
+    if (music) {
+        music.src = musicUrl;
+        music?.play().catch(err => console.log(err));
+    }
+
+    document.addEventListener('click', () => {
+        music?.play().catch(err => console.log(err));
     });
 });
